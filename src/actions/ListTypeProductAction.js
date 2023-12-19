f0867c85d06efaa7b276fc2d0f54e61e6f7372da
ThinhTran <thinhtran3803@gmail.com>
2023-12-19 23:05:50 +0700

    addd

--- src/actions/ListTypeProductAction.js
+++ src/actions/ListTypeProductAction.js
@@ -1,25 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const getAllTypeProduct = () => async (dispatch) => {
-    try {
-        const {data} = await axios.get('http://localhost:4000/typeList')
-        dispatch({type: 'GET_ALL_TYPE_PRODUCT', payload: data})
-    } catch (error) {
-    }
-}
+export const getAllTypeProduct = () => async dispatch => {
+	try {
+		const { data } = await axios.get(
+			"https://be-production-05ac.up.railway.app/typeList"
+		);
+		dispatch({ type: "GET_ALL_TYPE_PRODUCT", payload: data });
+	} catch (error) {}
+};
 
-export const CreateNewTypeProduct = (type) => async (dispatch) => {
-    try {
-        const {data} = await axios.post(`http://localhost:4000/typeList/create`, type)
-        dispatch({type: 'CREATE_NEW_TYPE_PRODUCT', payload: data})
-    } catch (error) {
-    }
-}
+export const CreateNewTypeProduct = type => async dispatch => {
+	try {
+		const { data } = await axios.post(
+			`https://be-production-05ac.up.railway.app/typeList/create`,
+			type
+		);
+		dispatch({ type: "CREATE_NEW_TYPE_PRODUCT", payload: data });
+	} catch (error) {}
+};
 
-export const deleteTypeProduct = (type) => async (dispatch) => {
-    try {
-        const {data} = await axios.delete(`http://localhost:4000/typeList/delete/${type._id}`)
-        dispatch({type: 'DELETE_TYPE_PRODUCT', payload: data})
-    } catch (error) {
-    }
-}
+export const deleteTypeProduct = type => async dispatch => {
+	try {
+		const { data } = await axios.delete(
+			`https://be-production-05ac.up.railway.app/typeList/delete/${type._id}`
+		);
+		dispatch({ type: "DELETE_TYPE_PRODUCT", payload: data });
+	} catch (error) {}
+};
