a660b4a0c07025d16ac0123762874c424a655a1c
ThinhTran <thinhtran3803@gmail.com>
2023-12-20 12:24:54 +0700

    'modifi'

--- src/App.js
+++ src/App.js
@@ -1,82 +1,91 @@
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import ProductPage from './pages/ProductPage';
-import DetailPage from './pages/DetailPage';
-import CartPage from './pages/CartPage';
-import OrderPage from './pages/OrderPage';
-import SearchPage from './pages/SearchPage';
-import AdminPage from './pages/AdminPage';
-import ResetScroll from './components/ResetScroll/ResetScroll';
-import MyOrderPage from './pages/MyOrderPage';
-import ChatPage from './pages/ChatPage';
-import PaymentPage from './pages/PaymentPage';
-import OrderSuccessPage from './pages/OrderSuccessPage'
+import React from "react";
+import "./App.css";
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProductPage from "./pages/ProductPage";
+import DetailPage from "./pages/DetailPage";
+import CartPage from "./pages/CartPage";
+import OrderPage from "./pages/OrderPage";
+import SearchPage from "./pages/SearchPage";
+import AdminPage from "./pages/AdminPage";
+import ResetScroll from "./components/ResetScroll/ResetScroll";
+import MyOrderPage from "./pages/MyOrderPage";
+import ChatPage from "./pages/ChatPage";
+import PaymentPage from "./pages/PaymentPage";
+import OrderSuccessPage from "./pages/OrderSuccessPage";
 
 function App() {
-  return (
-    <div className="App">
-  
-      <Router>
-        
-        <ResetScroll></ResetScroll>
+	return (
+		<div className="App">
+			<Router>
+				<ResetScroll />
 
-        <Route path="/" exact>
-          <HomePage></HomePage>
-        </Route>
+				<Switch>
+					<Route path="/" exact>
+						<HomePage />
+					</Route>
 
-        <Route path="/login">
-          <LoginPage></LoginPage>
-        </Route>
-        <Route path="/register">
-          <SignupPage></SignupPage>
-        </Route>
+					<Route path="/login" exact>
+						<LoginPage />
+					</Route>
 
-        <Route path="/product">
-          <ProductPage></ProductPage>
-        </Route>
-        <Route path="/detail/:id">
-          <DetailPage></DetailPage>
-        </Route>
+					<Route path="/register" exact>
+						<SignupPage />
+					</Route>
 
-        <Route path='/cart'>
-          <CartPage></CartPage>
-        </Route>
+					<Route path="/product" exact>
+						<ProductPage />
+					</Route>
 
-        <Route path='/order'>
-          <OrderPage></OrderPage>
-        </Route>
-        <Route path='/orderSuccess'>
-          <OrderSuccessPage></OrderSuccessPage>
-        </Route>
-        <Route path='/payment'>
-          <PaymentPage></PaymentPage>
-        </Route>
-        <Route path='/MyOrder'>
-          <MyOrderPage></MyOrderPage>
-        </Route>
+					<Route path="/detail/:id" exact>
+						<DetailPage />
+					</Route>
 
-        <Route path='/search'>
-          <SearchPage></SearchPage>
-        </Route>
+					<Route path="/cart" exact>
+						<CartPage />
+					</Route>
 
-        <Route path='/chat'>
-          <ChatPage></ChatPage>
-        </Route>
+					<Route path="/order" exact>
+						<OrderPage />
+					</Route>
 
-        <Route path='/admin'>
-          <AdminPage></AdminPage>
-        </Route>
+					<Route path="/orderSuccess" exact>
+						<OrderSuccessPage />
+					</Route>
 
-        {/* <Route path='*'>
-          <HomePage></HomePage>
-        </Route> */}
+					<Route path="/payment" exact>
+						<PaymentPage />
+					</Route>
 
-      </Router>
-    </div>
-  );
+					<Route path="/MyOrder" exact>
+						<MyOrderPage />
+					</Route>
+
+					<Route path="/search" exact>
+						<SearchPage />
+					</Route>
+
+					<Route path="/chat" exact>
+						<ChatPage />
+					</Route>
+
+					<Route path="/admin" exact>
+						<AdminPage />
+					</Route>
+
+					{/* Redirect for unknown routes */}
+					<Redirect to="/" />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
